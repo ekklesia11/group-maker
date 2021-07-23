04d4a7f6006e6b2e93d04fe1feaa41a09972aa8b
front_박찬현 <happyyquokka@cashwalk.io>
2021-07-23 09:59:33 +0900

    useLocalStorage cutom hook updated

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -4,14 +4,29 @@ const getCurrentLocalStorageList = () => {
   const list = localStorage.getItem('list');
   if (list) return JSON.parse(list);
   return [];
-}
+};
 
-const useLocalStorage = (name) => {
+const saveNewListIntoLocalStorage = (list) => {
+  const newList = JSON.stringify(list);
+  const res = localStorage.setItem('list', newList);
+  return true;
+};
+
+const nameValidation = (name) => {
+  if (!name) return false;
+
+  const typeStr = typeof name === 'string';
+  const typeNum = typeof name === 'number';
+  const isEmpty = typeStr && name.trim() === '';
+  
+  if (typeNum) return true;
+  return typeStr && !isEmpty;
+};
+
+const useLocalStorage = (name = null) => {
   const [list, setList] = useState(getCurrentLocalStorageList());
-  
-  setList(prev => prev.slice().push(name));
-  
-  
+
+  if (nameValidation(name)) setList(prev => [...prev, name]);
   return list;
 }
 
